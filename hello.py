45111d1b35fb1c861a2ccb19c624581e44d093df
hyun14159 <hyun.14159@gmail.com>
2021-01-29 11:13:54 +0900

    feat:ad if statement
    to print only if i is even

--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
-for _ in range(5):
-	print('hello, git!')
+from time import sleep, ctime
+
+def error_report():
+	fast_hackers = 404
+	print("{}".format(ctime()))
+	sleep(1)
+	print("hyun.14159@gmail.com{}".format())
+	sleep(1)
+	print("환경에 접속하는 중".format(ctime))
+	sleep(2)
+	print("해킹 시도 중{}".format(ctime))
+	sleep(2)
+	print("네트우커 서버 동기화{}".format(ctime))
+	sleep(2)
+	print("동기화 완료{}".format(ctime))
+
+	print("I'm done, this network is yours")
+
+	return fast_hackers
 
